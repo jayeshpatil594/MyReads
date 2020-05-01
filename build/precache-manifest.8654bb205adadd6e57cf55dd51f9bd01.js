d4e8bace25b2fdc09b584dbc2a6ba57c022a8e7a
Jayesh Patil <49228384+jayeshpatil594@users.noreply.github.com>
2020-05-02 01:31:41 +0530

    Restructure folders and Components

---
+++ build/precache-manifest.8654bb205adadd6e57cf55dd51f9bd01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80492c5c81a70438f3e908eed23c7be6",
+    "url": "/myreads/index.html"
+  },
+  {
+    "revision": "814d89b756316894dd5b",
+    "url": "/myreads/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4757fcfaa5dc9674f227",
+    "url": "/myreads/static/js/2.db4bd0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreads/static/js/2.db4bd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814d89b756316894dd5b",
+    "url": "/myreads/static/js/main.5b4c026b.chunk.js"
+  },
+  {
+    "revision": "6259aadd35d1b500b365",
+    "url": "/myreads/static/js/runtime-main.adac3c9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreads/static/media/logo.5d5d9eef.svg"
+  }
+]);
